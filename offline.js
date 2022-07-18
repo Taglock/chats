5356a902ce6f6c9c3a2ad312af32b9a58d42d44e
Taglock <39584594+Taglock@users.noreply.github.com>
2022-07-18 13:11:39 +0500

    common

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1658131719,
+	"fileList": [
+		"data.js",
+		"game.html",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"style.css",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
